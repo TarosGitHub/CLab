53ae43432a11c8b89ac316287e512c6b42f6d30a
Taro Hoshino <no_taro1990@outlook.com>
2020-06-25 00:29:25 +0900

    Fixed comments
    Stack -> stack

--- vs/Algorithms_And_Data_Structures/Stack/Stack.h
+++ vs/Algorithms_And_Data_Structures/Stack/Stack.h
@@ -22,80 +22,81 @@ enum Stack_status {
  */
 struct Stack {
 	unsigned int elem_size; /**< Size of an element. */
-	char* memory; /**< Memory area of Stack. */
-	unsigned int capacity; /**< Maximum number of elements on Stack. */
-	unsigned int pointer; /**< Pointer to top of Stack. */
+	char* memory; /**< Memory area of stack. */
+	unsigned int capacity; /**< Maximum number of elements on stack. */
+	unsigned int pointer; /**< Pointer to top of stack. */
 };
 
 /**
- * Create Stack.
+ * Create a stack.
  * @see Test_Stack_new
  * @param elem_size Size of an element.
- * @param memory Memory area of Stack.
- * @param capacity Maximum number of elements on Stack.
- * @return Return one Stack object.
+ * @param memory Memory area of stack.
+ * @param capacity Maximum number of elements on stack.
+ * @return Return a stack object.
  */
 #define Stack_new(elem_size, memory, capacity) {sizeof(elem_size), (char*)(memory), (capacity), 0U}
 
 /**
- * Initialize Stack.
+ * Initialize the stack.
  * @see Test_Stack_init
- * @param stack Pointer to Stack.
+ * @param stack Pointer to stack.
  * @param elem_size Size of an element.
- * @param memory Memory area of Stack.
- * @param capacity Maximum number of elements on Stack.
+ * @param memory Memory area of stack.
+ * @param capacity Maximum number of elements on stack.
  */
 extern void Stack_init(struct Stack* stack, unsigned int elem_size, void* memory, unsigned int capacity);
 
 /**
- * Determine if Stack is empty.
+ * Determine if stack is empty.
  * @see Test_Stack_is_empty
- * @param stack Pointer to Stack.
- * @return Return true if Stack is empty, otherwise return false.
+ * @param stack Pointer to stack.
+ * @return Return true if stack is empty, otherwise return false.
  */
 extern enum Stack_bool Stack_is_empty(struct Stack* stack);
 
 /**
- * Determine if Stack is full.
+ * Determine if stack is full.
  * @see Test_Stack_is_full
- * @param stack Pointer to Stack.
- * @return Return true if Stack is full, otherwise return false.
+ * @param stack Pointer to stack.
+ * @return Return true if stack is full, otherwise return false.
  */
 extern enum Stack_bool Stack_is_full(struct Stack* stack);
 
 /**
- * Get the number of elements on Stack.
+ * Get the number of elements on stack.
  * @see Test_Stack_size
- * @param stack Pointer to Stack.
- * @return Return the number of elements on Stack.
+ * @param stack Pointer to stack.
+ * @return Return the number of elements on stack.
  */
 extern unsigned int Stack_size(struct Stack* stack);
 
 /**
- * Push an element to Stack.
+ * Push an element to stack.
  * @see Test_Stack_push
- * @param stack Pointer to Stack.
- * @param elem An element that is pushed to Stack(You must not set NULL to this argument).
- * @return Return STACK_FAILURE if Stack is full, otherwise return STACK_SUCCESS.
+ * @param stack Pointer to stack.
+ * @param elem An element that is pushed to stack(You must not set NULL to this argument).
+ * @return Return STACK_FAILURE if stack is full, otherwise return STACK_SUCCESS.
  */
 extern enum Stack_status Stack_push(struct Stack* stack, const void* elem);
 
 /**
- * Pop an element from Stack.
+ * Pop an element from stack.
  * @see Test_Stack_pop
- * @param stack Pointer to Stack.
- * @param elem An element that is popped from Stack(You can set NULL to this argument. In that case, no value is set to this argument).
- * @return Return STACK_FAILURE if Stack is empty, otherwise return STACK_SUCCESS.
+ * @param stack Pointer to stack.
+ * @param elem An element that is popped from stack(You can set NULL to this argument. In that case, no value is set to this argument).
+ * @return Return STACK_FAILURE if stack is empty, otherwise return STACK_SUCCESS.
  */
 extern enum Stack_status Stack_pop(struct Stack* stack, void* elem);
 
 /**
- * Copy Stack.
- * This function makes a deep copy of Stack memory area.
- * The copy destination stack memory area must be larger than or equal to the copy source stack memory area.
+ * Copy stack.
+ * This function makes a deep copy of stack memory area.
+ * The element size of destination stack must be as large as the element size of source stack.
+ * If the size of the memory area is different, it will be copied to fit the smaller memory area.
  * @see Test_Stack_copy
- * @param dst Destination to copy
- * @param src Source of copy
+ * @param dst Destination stack
+ * @param src Source stack
  */
 extern void Stack_copy(struct Stack* dst, const struct Stack* src);
 
