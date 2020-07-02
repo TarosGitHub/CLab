a65201ed69df6bd1a397b552abfbf06ef14bd249
Taro Hoshino <no_taro1990@outlook.com>
2020-07-02 19:04:06 +0900

    Added LinkedListCell

---
+++ vs/Algorithms_And_Data_Structures/LinkedList/LinkedListCell.h
@@ -0,0 +1,4 @@
+#if !defined(LINKEDLISTCELL_H)
+#define LINKEDLISTCELL_H
+
+#endif /* LINKEDLISTCELL_H */
---
+++ vs/Algorithms_And_Data_Structures/Test_LinkedList/Test_LinkedListCell.cpp
@@ -0,0 +1,20 @@
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "LinkedListCell.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestLinkedListIterator
+{
+	TEST_CLASS(Test)
+	{
+	public:
+
+		TEST_METHOD(normal)
+		{
+			//
+		}
+	};
+}
