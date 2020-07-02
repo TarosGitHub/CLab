514c4d1d1520d7dc254b2d675218b99d00e09564
Taro Hoshino <no_taro1990@outlook.com>
2020-07-02 19:00:02 +0900

    Making LinkedListIterator_has_next

--- vs/Algorithms_And_Data_Structures/LinkedList/LinkedListIterator.c
+++ vs/Algorithms_And_Data_Structures/LinkedList/LinkedListIterator.c
@@ -1,3 +1,7 @@
+#include <stddef.h>
 #include "LinkedListIterator.h"
 
-
+enum Boolean LinkedListIterator_has_next(struct LinkedListIterator* iterator)
+{
+	return NULL != iterator->current->next ? TRUE : FALSE;
+}
--- vs/Algorithms_And_Data_Structures/LinkedList/LinkedListIterator.h
+++ vs/Algorithms_And_Data_Structures/LinkedList/LinkedListIterator.h
@@ -1,6 +1,8 @@
 #if !defined(LINKEDLISTITERATOR_H)
 #define LINKEDLISTITERATOR_H
 
+#include "Common.h"
+
 struct LinkedListCell;
 
 /**
@@ -10,5 +12,7 @@ struct LinkedListIterator {
 	struct LinkedListCell* current; /**< The pointer to the current cell element. */
 	struct LinkedListCell* previous; /**< The pointer to the previous of the current cell element. */
 };
+
+extern enum Boolean LinkedListIterator_has_next(struct LinkedListIterator* iterator);
 
 #endif /* LINKEDLISTITERATOR_H */
